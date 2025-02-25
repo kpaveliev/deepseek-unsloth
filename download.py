9d13d903a14391071344a8c7c1aa80062d79e094
kpaveliev <kpaveliev@gmail.com>
2025-02-25 13:02:46 +0300

    update

--- download.py
+++ download.py
@@ -2,8 +2,12 @@
 # os.environ["HF_HUB_ENABLE_HF_TRANSFER"] = "1"
 
 from huggingface_hub import snapshot_download
-snapshot_download(
-  repo_id = "unsloth/DeepSeek-R1-GGUF",
-  local_dir = "DeepSeek-R1-GGUF",
-  allow_patterns = ["*UD-IQ1_S*"], # Select quant type UD-IQ1_S for 1.58bit
-)
+
+def donwload_unsloth():
+  snapshot_download(
+    repo_id = "unsloth/DeepSeek-R1-GGUF",
+    local_dir = "DeepSeek-R1-GGUF",
+    allow_patterns = ["*UD-IQ1_S*"], # Select quant type UD-IQ1_S for 1.58bit
+  )
+
+
