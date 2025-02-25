90d4047cfd659b3d3959ef4c22de0abe697ddbdc
kpaveliev <kpaveliev@gmail.com>
2025-02-25 14:07:32 +0300

    proxy settings

--- download.py
+++ download.py
@@ -14,7 +14,8 @@ def donwload_unsloth():
     snapshot_download(
         repo_id = "unsloth/DeepSeek-R1-GGUF",
         local_dir = "DeepSeek-R1-GGUF",
-        allow_patterns = ["*UD-IQ1_S*"], # Select quant type UD-IQ1_S for 1.58bit
+        allow_patterns = ["*UD-IQ1_S*"],
+        proxies={'https': os.getenv('HTTPS_PROXY')}  # Select quant type UD-IQ1_S for 1.58bit
     )
 
 if __name__ == "__main__": 
